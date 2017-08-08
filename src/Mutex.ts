9b579cdbf9beca6c73cdc2404555d8f792b42cd2
Spencer Killen <spleen_387@me.com>
2017-08-08 11:31:11 -0600

    Check mutex owner

--- src/Mutex.ts
+++ src/Mutex.ts
@@ -5,30 +5,38 @@ const enum State {
 
 export class Mutex {
   private state: Int32Array;
+  private isOwner: boolean;
   constructor(buff: SharedArrayBuffer, offset: number) {
     // TODO Atomics.isLockFree()?
     this.state = new Int32Array(buff, offset, 1);
   }
-  lock(): true {
-  for (; ;) {
+  lock() {
+    if (this.isOwner) {
+      throw new Error("Thread tried to lock mutex twice");
+    }
+    for (; ;) {
+      const old = Atomics.compareExchange(this.state, 0, State.unlocked, State.locked);
+      if (old == State.unlocked) {
+        this.isOwner = true;
+        return;
+      } else {
+        Atomics.wait(this.state, 0, State.locked);
+      }
+    }
+  }
+
+  unlock() {
+    if (!this.isOwner) {
+      throw new Error("Attempt to unlock mutex that is not owned by thread")
+    }
+    Atomics.store(this.state, 0, State.unlocked);
+  }
+  tryLock(): boolean {
     const old = Atomics.compareExchange(this.state, 0, State.unlocked, State.locked);
     if (old == State.unlocked) {
       return true;
-    } else {
-      Atomics.wait(this.state, 0, State.locked);
     }
+    return false;
   }
-}
-
-unlock() {
-  Atomics.store(this.state, 0, State.unlocked);
-}
-tryLock(): boolean {
-  const old = Atomics.compareExchange(this.state, 0, State.unlocked, State.locked);
-  if (old == State.unlocked) {
-    return true;
-  }
-  return false;
-}
-readonly sizeof = 8;
+  readonly sizeof = 8;
 }
