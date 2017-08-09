839acd9f69083ee79bb30a4557824704e03bd27d
Spencer Killen <spleen_387@me.com>
2017-08-09 08:57:26 -0600

    fixup! Enable strict mode, share setup across tests

--- tests/setup.js
+++ tests/setup.js
@@ -1,28 +1,31 @@
-window.getSetup = function (workerFile) {
-   return function setup(test, sab, numWorkers = 1) {
-      const workers = []
-      let finished = 0
-      return new Promise((resolve, reject) => {
-         for (x = 0; x < numWorkers; x++) {
-            nw = new Worker(workerFile)
-            nw.postMessage({
-               command: test,
-               data: sab
-            });
-            workers.push(nw);
-            nw.addEventListener("message", msg => {
-               if (msg.data === "done") {
-                  finished++;
-                  if (finished == numWorkers) {
-                     resolve();
+(function () {
+   "use strict";
+   window.getSetup = function (workerFile) {
+      return function setup(test, sab, numWorkers = 1) {
+         const workers = []
+         let finished = 0
+         return new Promise((resolve, reject) => {
+            for (x = 0; x < numWorkers; x++) {
+               nw = new Worker(workerFile)
+               nw.postMessage({
+                  command: test,
+                  data: sab
+               });
+               workers.push(nw);
+               nw.addEventListener("message", msg => {
+                  if (msg.data === "done") {
+                     finished++;
+                     if (finished == numWorkers) {
+                        resolve();
+                     }
+                  } else if (msg.data === "fail") {
+                     reject();
+                  } else {
+                     throw new Error("Invalid message from thread");
                   }
-               } else if (msg.data === "fail") {
-                  reject();
-               } else {
-                  throw new Error("Invalid message from thread");
-               }
-            })
-         }
-      })
-   };
-}
+               })
+            }
+         })
+      };
+   }
+}())
